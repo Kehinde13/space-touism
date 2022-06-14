189d0e7d65d1b6d3d2bca335e23a2486eaf595f9
kehinde <balogunkehinde3@gmail.com>
2022-06-14 09:24:53 -0500

    fixed a bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes  } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes  } from "react-router-dom";
 import { Header, Home } from './components'
 import { Destination, Crew, Technology } from './pages'
 
@@ -8,7 +8,7 @@ function App() {
     <BrowserRouter>
        <Header />
 
-       <Home />
+      
        
        <Routes>
          <Route path="/" element={<Home />} />
@@ -18,6 +18,8 @@ function App() {
          <Route path="/Crew" element={<Crew />} />
            
          <Route path="/Technology" element={<Technology />} />
+
+         <Route path="*" element={<Navigate to="/" replace />} />
            
        </Routes>
     </BrowserRouter>
